bb0131730f9d41aad5cd2a400e981536e86018e7
Roman Petrakovskiy <105650706+s1g11@users.noreply.github.com>
2024-02-14 15:46:32 +0700

    add-val

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import s from './App.module.css'
+import { rand_nums } from "./rand_nums"
 
-function App() {
+export function App() {
+  const [pos, setPos] = useState([0,0])
+  const [yes, setYes] = useState(false)
+  const [index, setIndex] = useState(0)
   const [count, setCount] = useState(0)
+  
+  const enterHandler = () => {
+    setCount(count+1)
+    const modx = Math.floor(Math.random() * (4)) + 1
+    const mody = Math.floor(Math.random() * (4)) + 1
+    const x = Math.floor(Math.random() * (700 - 50 + 1)) + 50
+    const y = Math.floor(Math.random() * (250 + 50 + 1)) + 50
+    setPos([modx === 1 ? -x : x, mody === 1 ? -y : y])
+  }
+
+  const hs = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+  // const hs = [1]
+  const vs = ['src/assets/valentinki/1.png','src/assets/valentinki/2.png','src/assets/valentinki/3.png','src/assets/valentinki/4.png',
+  'src/assets/valentinki/5.png','src/assets/valentinki/6.png','src/assets/valentinki/7.png','src/assets/valentinki/8.png',
+  'src/assets/valentinki/9.png','src/assets/valentinki/10.png','src/assets/valentinki/11.png','src/assets/valentinki/12.png',
+  'src/assets/valentinki/13.png','src/assets/valentinki/14.png','src/assets/valentinki/15.png','src/assets/valentinki/16.png',
+  'src/assets/valentinki/17.png','src/assets/valentinki/18.png','src/assets/valentinki/19.png','src/assets/valentinki/20.png',
+  'src/assets/valentinki/21.png','src/assets/valentinki/22.png','src/assets/valentinki/23.png','src/assets/valentinki/24.png',
+  'src/assets/valentinki/25.png','src/assets/valentinki/26.png','src/assets/valentinki/27.png','src/assets/valentinki/28.png',
+  'src/assets/valentinki/29.png','src/assets/valentinki/30.png']
+
+  if (yes) {
+    setTimeout(()=>setIndex(index+1), 100)
+   
+    return <>
+      {/* <div className={s.vals}> */}
+        {vs.map((v, i) => {
+
+          return <div className={s.val} style={{backgroundImage: `url(${v})`, left: rand_nums[i], top: rand_nums[rand_nums.length - i]}} ></div>
+        })}
+      {/* </div> */}
+      {hs.map(v => {
+        const a = v
+        const x = Math.floor(Math.random() * (1800 + 1))
+        const y = Math.floor(Math.random() * (700 + 1))
+        return <div className={s.heart} style={{backgroundImage: 'url(/src/assets/1.png)', left: x, top: y}} ></div>
+      })}
+    </>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={s.container}>
+      <p>Янни (Кави), ты принимаешь валентинку от меня (Рома (Романя (Ромашка)))???😘😏❤️😭🤨</p>
+      <div className={s.buttons}>
+        {count >= 2 && <a onClick={()=>setYes(true)} className={s.da + ' ' + s.button}>Да!</a>}
+        <a onClick={()=>alert("ээ так не договаривались!! переигрывай")} className={s.button + ' ' + s.no} style={{left: pos[0], top: pos[1]}} onMouseEnter={enterHandler}>Неа)</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
